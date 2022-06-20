caec0c5f1c94e156d8363e9f35d71b219616fd05
Liam <93374467+william-4@users.noreply.github.com>
2022-06-20 08:40:31 +0300

    add description

--- pchar.c
+++ pchar.c
@@ -1,5 +1,14 @@
 #include "monty.h"
 
+
+/**
+ * pchar - prints a char
+ * @stack: the stack passed by main
+ * @line_cnt: the line number
+ *
+ * Return: void
+ */
+
 void pchar(stack_t **stack, unsigned int line_cnt)
 {
 	(void)stack;
