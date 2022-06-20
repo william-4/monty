10f96167118ea90b156f80f32b8c2661ec7ec211
Liam <93374467+william-4@users.noreply.github.com>
2022-06-20 08:44:25 +0300

    add description

--- rotr.c
+++ rotr.c
@@ -1,5 +1,14 @@
 #include "monty.h"
 
+
+/**
+ * rotr - rotates the stack
+ * @stack: the stack passed by main
+ * @line_cnt: the line number
+ *
+ * Return: void
+ */
+
 void rotr(stack_t **stack, unsigned int line_cnt)
 {
 	(void)stack;
