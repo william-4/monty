498dde10d736e1881d4b64ac078c3017716e9612
Liam <93374467+william-4@users.noreply.github.com>
2022-06-20 08:42:19 +0300

    add description

--- pstr.c
+++ pstr.c
@@ -1,5 +1,14 @@
 #include "monty.h"
 
+
+/**
+ * pstr - prints a string
+ * @stack: the stack passed by main
+ * @line_cnt: the line number
+ *
+ * Return: void
+ */
+
 void pstr(stack_t **stack, unsigned int line_cnt)
 {
 	(void)stack;
